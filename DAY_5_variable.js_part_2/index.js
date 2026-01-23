ac44ee2e57b77576280fd3248c5618c48f5faa24
Anand <anandnishad665583@gmail.com>
2026-01-23 15:10:59 +0530

    today learn deep variable

---
+++ DAY_5_variable.js_part_2/index.js
@@ -0,0 +1,22 @@
+// MY LEARNING CODE---
+// TODAY LEARN VARIABLE PART 2
+// LET
+let product = "computer";
+// product = "mouse"; Ham is value ko badal sakte hai ;
+// let product = "mouse"; ham fhir se let me variable define nahi kar sakte;
+console.log(product);
+// var
+// ye globle variable hai;
+var price = 10;
+var price = 5;
+
+// use many times
+// CONST
+const storage = 64;
+// const storage = 34; iski value fix ho jati hai fhir se badal nahi sakte:
+
+// Calculation Logic (Using variables)
+let myBalance = 500;
+let chocolatePrice = 100;
+let remaining = myBalance - chocolatePrice;
+console.log(`paisa bacha:${remaining}`);
