31510f44ea90a5afb33c58db8776b6888f69a3f0
Anand <anandnishad665583@gmail.com>
2026-01-24 14:28:35 +0530

    today i learn datatype and practise

---
+++ DAY_6_DATATYPE/index.js
@@ -0,0 +1,40 @@
+// TODAY I LEARN DATATYPE
+// NUMBER
+let number = 234;
+console.log(typeof(number));
+// NULL
+let box = null;
+console.log(typeof(box));
+// STRING
+let name = "Anand";
+console.log(typeof(name));
+// SYMBOL
+let bhai1 = Symbol("Anand");
+let bhai2= Symbol("Anand");
+console.log(bhai1 === bhai2); 
+// output false
+
+// BOOLEAN
+let value = false;
+console.log(typeof(value));
+// BIGINT
+let bignum = 123445454n;
+console.log(typeof(bignum))
+// UNDEFINED
+let product;
+console.log(typeof(product));
+
+
+// DYNAMICALLY TYPED LAUNGUAGE
+// ham jab jab value update karenge typeof change hoga;
+let a = 2;
+console.log(typeof (a));
+a = "hello";
+console.log(typeof(a));
+a = true;
+console.log(typeof(a));
+a = null;
+console.log(typeof(a));
+
+
+
