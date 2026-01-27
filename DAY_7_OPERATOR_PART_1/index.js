ada7de8c3f198f95e9b9e2e671cc61be4388ad6f
Anand <anandnishad665583@gmail.com>
2026-01-27 14:27:55 +0530

    today i learn operator

---
+++ DAY_7_OPERATOR_PART_1/index.js
@@ -0,0 +1,48 @@
+// Arithmetic operator
+console.log("hello");
+console.log(5+3);
+console.log(5-3);
+console.log(5*3);
+console.log(5/3);
+console.log(5**3)
+
+// Assignment
+let a = 10;
+a = a + 5;
+console.log(a);
+a += 5;
+console.log(a);
+a -=5;
+console.log(a);
+a /=5;
+console.log(a);
+a %=5;
+console.log(a);
+a **=5;
+console.log(a)
+// comparison operator
+console.log(5 > 2);
+console.log(5 < 2);
+console.log(5 ==3);
+console.log(5 =="5");
+console.log(5 === 5);
+console.log(5 === "5");
+console.log(5!==3);
+console.log(5!=="3");
+// logical oprator
+// and &&
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+// or ||
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+// !
+console.log(!true)
+console.log(!false)
+
+
+
