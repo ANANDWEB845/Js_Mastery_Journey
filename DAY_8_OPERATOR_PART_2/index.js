c4d242c9375d2a2ace40e9d0e214c3ff794e83d3
Anand <anandnishad665583@gmail.com>
2026-01-28 15:10:57 +0530

    today learn operator part 2

---
+++ DAY_8_OPERATOR_PART_2/index.js
@@ -0,0 +1,36 @@
+// increment / decrement operator;
+let a = 5;
+a++;
+console.log(a);
+a++;
+console.log(a);
+a--;
+console.log(a);
+++a;
+console.log(a);
+--a;
+console.log(a);
+
+// ternary 
+let age = 22;
+let result = ((age > 18)? "drive cae":"do not drive car");
+console.log(result);
+
+// bitwish
+// and
+console.log(5 & 3);
+// ans = 1;
+console.log(4 & 2);
+// ans = 0;
+
+// or
+console.log(5 | 3);
+// ans = 7;
+console.log(4 | 2);
+// ans = 6;
+
+// NOT
+console.log(~4);
+// ans = -5;
+console.log(~7);
+// ans = -8;
