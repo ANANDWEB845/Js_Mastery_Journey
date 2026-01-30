15395466871f0588f4505f6ad699a89db489a636
Anand <anandnishad665583@gmail.com>
2026-01-30 13:31:33 +0530

    today i learn if else

---
+++ DAY_10_IF_ELSE_PART_1/index.js
@@ -0,0 +1,12 @@
+// if statement
+let age = 18;
+if(age >= 18){
+  console.log("you are drive")
+}
+// if else statement
+let price = 16;
+if(price >= 20){
+  console.log("able to buy this product");
+}else{
+  console.log("you are not able to buy this product");
+}
