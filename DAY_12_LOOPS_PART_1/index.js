c19cd21e19e0a1efd7b4e6661b6c595455a39be9
Anand <anandnishad665583@gmail.com>
2026-02-02 20:31:23 +0530

    today i lean loops

---
+++ DAY_12_LOOPS_PART_1/index.js
@@ -0,0 +1,30 @@
+// FOR LOOP
+
+// First Example
+for(let a = 5; a >=1; a--){
+  console.log(a)
+}
+// Second Example
+for(let t= 5;t<=50; t +=5){
+  console.log(t)
+}
+
+// WHILE LOOP
+
+// First Example
+let samoshakhaya = 0;
+while(samoshakhaya <=10){
+  console.log(samoshakhaya);
+  samoshakhaya++;
+}
+
+// Second Example
+let dice = 0;
+while (dice !== 6){
+  dice = Number(prompt("dice number dalo 1 to 6" ));
+  
+  console.log("maine diya : ",dice);
+}
+console.log("goti bahar nikal gai");
+
+
