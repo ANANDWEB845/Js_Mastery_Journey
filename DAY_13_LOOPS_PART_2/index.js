fbc773c779a9efd39ba898f06138d87e39c8fb64
Anand <anandnishad665583@gmail.com>
2026-02-03 20:21:20 +0530

    today i learn do-while-loop,for-of-loop,for-in-loop

---
+++ DAY_13_LOOPS_PART_2/index.js
@@ -0,0 +1,18 @@
+// DO WHILE LOOP
+let a = 1;
+do{
+  console.log(a);
+  a++;
+}while( a <= 20)
+
+// FOR OF LOOP
+let name = "Anand Nishad";
+for(let a of name){
+  console.log(a);
+}
+
+// FOR IN LOOP 
+let p = "Anand Nishad";
+for(let c in p){
+  console.log(c);
+}
